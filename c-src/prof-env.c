5fc524c0987a07301a771713c8928a18de4744f9
Bill Kozak <brkozak@gmail.com>
2026-05-24 15:14:52 +0000

    Config user socket path from env var

---
+++ c-src/prof-env.c
@@ -0,0 +1,30 @@
+/*
+ * This file is part of jauto-profiler
+ * Copyright (c) 2026 Bill Kozak
+ *
+ * This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "prof-env.h"
+
+#include <stdlib.h>
+
+const char *prof_socket_path(void)
+{
+	const char *path = getenv(SOCKET_ENV_VAR);
+	if (path != NULL) {
+		return path;
+	}
+	return "/tmp/jauto-profiler.sock";
+}
